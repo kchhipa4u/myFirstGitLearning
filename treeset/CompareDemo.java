2c736178cc988828d9636146fe1f54ddc23bc9ca
Kanhaiya Chhipa <kchhipa4u@gmail.com>
2018-09-05 06:33:27 +0530

    This is my first commit.

---
+++ treeset/CompareDemo.java
@@ -0,0 +1,10 @@
+package day55.collectionframework.set.treeset;
+
+public class CompareDemo {
+
+	public static void main(String[] args) {
+		System.out.println("A".compareTo("B"));  // -ve
+		System.out.println("D".compareTo("B"));  // +ve
+		System.out.println("D".compareTo("D"));  // equal
+	}
+}
---
+++ treeset/SecondFile.java
@@ -0,0 +1,8 @@
+
+public class SecondFile {
+
+	public static void main(String[] args) {
+		System.out.println("Git Learning");
+        System.out.println("another line");		
+	}
+}
